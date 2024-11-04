57c298808a2e868218b09f30cc02a94c548ab1f3
ajm <jeck77@naver.com>
2024-11-04 22:16:56 +0900

    다대다 정리

---
+++ src/main/java/hellojpa/JpaMain11.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+/**
+ * 다대다
+ */
+public class JpaMain11 {
+
+    public static void main(String[] args) {
+        // 하나만 생성해서 애플리케이션 전체에서 공유
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 쓰레드 간 공유 x
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
