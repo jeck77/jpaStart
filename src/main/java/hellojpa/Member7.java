11314489e19243a344b86908faa9b544b605851c
ajm <jeck77@naver.com>
2024-11-08 00:50:31 +0900

    프록시

---
+++ src/main/java/hellojpa/Member7.java
@@ -0,0 +1,49 @@
+package hellojpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+/**
+ * 프록시
+ */
+@Entity
+public class Member7 extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    @Column(name = "USERNAME")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team2 team2;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team2 getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(Team2 team2) {
+        this.team2 = team2;
+    }
+}
